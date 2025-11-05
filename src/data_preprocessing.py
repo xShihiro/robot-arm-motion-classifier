1607eedb640fbeced901e439baccaed2395324b6
Edwin Lippert <elippert@mail.uni-paderborn.de>
2025-11-05 22:48:13 +0900

    Add documentation for the tuple extraction helper method

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,12 +1,23 @@
 import os
 
+# lists to fill the corresponding data in
+circle_data = []
+diagonal_left_data = []
+diagonal_right_data = []
+horizontal = []
+vertical = []
+
 # extract the XYZ coordinates and create a list of ordered tuples
 def movement_into_tuple_list(filepath: str) -> list:
     res = []
+    
+    # create a list of all the lines in the file
     with open(filepath, "r") as file:
         content = file.read().replace("\n", "")
         lines = content.split("#")
-        
+    
+    # for each line extract the column with the coordinates, converts it into
+    # an int tuple and appends it to the result list    
     for line in lines:
         parts = line.split(",")
         if len(parts) > 6:
